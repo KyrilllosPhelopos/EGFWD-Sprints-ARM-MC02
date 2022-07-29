ef6f72a9f72c39d11ddea3c9b32321962d5e1bf2
Kyrillos <kyrillos@koko.com>
2022-07-29 22:27:30 +0200

    blinking led

--- Src/Appl/main.c
+++ Src/Appl/main.c
@@ -1,10 +1,21 @@
 #include "IntCtrl.h"
-
+#include "Mcu.h"
+#include "Port.h"
+#include "DIO.h"
+#include "Timer.h"
 int main(void)
 {
 	
+	Mcu_Init(&Mcu_ConfigTypeStruct);
+	Mcu_InitClock(ACTIVE_CLOCK_CONFIG_MODE);
+	Mcu_DistributePllClock();
 	IntCtrl_Init();
-	
-	while(1);
+	Port_Init(Port_Config);
+	Gpt_Init(Gpt_Config);
+	while(1){
+        Gpt_Init(Gpt_Config);
+		Gpt_EnableNotification(Gpt_Channel_0);
+        }
+        
 	return 0;
 }
